86747be8541bf383b365da9ee5823770a4539d52
edgarbjorntvedt <eb@24sevenoffice.com>
2018-12-21 16:20:44 +0100

    added more tests for swedish bban account numbers

--- test/SE/testBbanValidation.ts
+++ test/SE/testBbanValidation.ts
@@ -8,6 +8,60 @@ describe('SwedishBbanValidation', ()=> {
     })
 
     describe('when validating', ()=> {
+        it('should validate a valid Swedish BBAN account as string input', ()=>{
+            chai.expect(validation.validate({accountNumber: '54401122003'} /* prod */)).to.deep.equal({
+                'valid': true
+            })
+        })
+
+        it('should validate a valid Swedish BBAN account as object input', ()=>{
+            chai.expect(validation.validate({accountNumber: '54401122003'})).to.deep.equal({
+                'valid': true
+            })
+        })
+
+        it('should validate a valid type 1.1 Swedish BBAN account (SEB)', ()=>{
+            chai.expect(validation.validate({accountNumber: '54401122003'})).to.deep.equal({
+                'valid': true
+            })
+        })
+
+        it('should validate a valid type 1.1 Swedish BBAN account (danskebank)', ()=>{
+            chai.expect(validation.validate({accountNumber: '12811122001'})).to.deep.equal({
+                'valid': true
+            })
+        })
+        it('should validate a valid type 1.2 Swedish BBAN account (Dnb)', ()=>{
+            chai.expect(validation.validate({accountNumber: '91951122005'})).to.deep.equal({
+                'valid': true
+            })
+        })
+        it('should validate a valid type 1.2 Swedish BBAN account (Lansforsakringer WASA Bank)', ()=>{
+            chai.expect(validation.validate({accountNumber: '90231122006'})).to.deep.equal({
+                'valid': true
+            })
+        })
+        it('should validate a valid type 2.1 Swedish BBAN account (Sparebanken syd)', ()=>{
+            chai.expect(validation.validate({accountNumber: '95701122211004'})).to.deep.equal({
+                'valid': true
+            })
+        })
+        it('should validate a valid type 2.2 Swedish BBAN account (Handelsbanken)', ()=>{
+            chai.expect(validation.validate({accountNumber: '6000112221009'})).to.deep.equal({
+                'valid': true
+            })
+        })
+        it('should validate a valid type 2.3 Swedish BBAN account (Swedbank)', ()=>{
+            chai.expect(validation.validate({accountNumber: '83881122211004'})).to.deep.equal({
+                'valid': true
+            })
+        })
+
+        it('should invalidate an invalid Swedish BBAN account', ()=>{
+            chai.expect(validation.validate({accountNumber: '54401122003'})).to.deep.equal({
+                'valid': false
+            })
+        })
 
 
         it('should invalidate an unknown clearing number in a Swedish BBAN account (type 0.0)', ()=>{
