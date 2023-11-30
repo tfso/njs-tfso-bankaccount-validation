23fe048df1a18dbe49390aae7865177d9f8bc579
Edgar Bjørntvedt <edgar.bjorntvedt@gmail.com>
2023-11-30 23:45:17 +0100

    update CICD

---
+++ build.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+export dir=dist
+if [[ ! -e $dir ]]; then
+    mkdir $dir
+elif [[ ! -d $dir ]]; then
+    echo "$dir already exists but is not a directory" 1>&2
+fi
+
+rm -rf ./dist/*
+
+tsc
+
+#cp -r ./types ./dist/
+#cp -r ./src ./dist/src/
+cp README.md ./dist/
+cp package.json ./dist/
+cp index.js ./dist/
